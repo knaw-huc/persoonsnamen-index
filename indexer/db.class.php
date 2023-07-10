6c6564ded376f5f5008dc035d45936a3f533aa31
meindertkroese <>
2023-07-10 16:51:28 +0200

    first adjustments

--- indexer/db.class.php
+++ indexer/db.class.php
@@ -6,37 +6,17 @@ class db
 
     function __construct()
     {
-        $this->con = pg_connect("host=localhost port=5432 dbname=sport_ing user=robzeeman password=bonzo");
+        $this->con = pg_connect("host=localhost port=5432 dbname=persoonsnamenindex user=robzeeman password=bonzo");
     }
 
     function get_items($count)
     {
-        $result = pg_query($this->con, "SELECT id, plaats, naam, DATE_PART('year' ,begindatum) as beginjaar, begindatum_soort, DATE_PART('year', einddatum) AS eindjaar, einddatum_soort, levensbeschouwing  FROM vereniging_vereniging LIMIT $count");
+        $result = pg_query($this->con, "SELECT _id, name, infix, givenname, life_hint_begin, life_hint_end  FROM persons LIMIT $count");
         return $this->ass_arr($result);
     }
 
-    function get_sports($id) {
-        $result = pg_query($this->con, "SELECT soort_sport AS sport FROM vereniging_sport WHERE vereniging = $id");
-        return $this->ass_arr($result);
-    }
-
-    function get_province($id) {
-        $result = pg_query($this->con, "SELECT provincie FROM vereniging_provincie WHERE vereniging = $id");
-        $prov = $this->ass_arr($result);
-        if (count($prov)) {
-            return $prov[0]["provincie"];
-        } else {
-            return "";
-        }
-    }
-
-    function get_landelijk($id) {
-        $result = pg_query($this->con, "SELECT naam AS bond FROM landelijke_bond WHERE landelijke_bond = $id");
-        return $this->ass_arr($result);
-    }
-
-    function get_lokaal($id) {
-        $result = pg_query($this->con, "SELECT naam AS bond FROM regionale_bond WHERE regionale_bond = $id");
+    function get_databse($id) {
+        $result = pg_query($this->con, "SELECT _id,name FROM database);
         return $this->ass_arr($result);
     }
 
